597f022fad5476b894762aa773c3d38ca28bd05e
ADDEPALLIOMKAR <omkaraddepalli@gmail.com>
2025-11-14 16:40:35 +0530

    6th commit

--- src/main/java/com/fortunebank/config/DatabaseConnectionChecker.java
+++ src/main/java/com/fortunebank/config/DatabaseConnectionChecker.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
